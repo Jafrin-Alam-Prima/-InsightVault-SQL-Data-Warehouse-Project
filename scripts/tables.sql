74aabff08ba676917d0805b3993cec348ab53fb3
Jafrin Alam Prima <121804372+Jafrin-Alam-Prima@users.noreply.github.com>
2026-04-01 14:44:37 +0600

    Update tables.sql

--- scripts/tables.sql
+++ scripts/tables.sql
@@ -1,4 +1,7 @@
--- crm_cust_info
+-- =================================
+-- CREATE TABLES FIRST
+-- =================================
+
 DROP TABLE IF EXISTS bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info(
     cst_id INT,
@@ -10,7 +13,6 @@ CREATE TABLE bronze.crm_cust_info(
     cst_create_date DATE
 );
 
--- crm_prd_info
 DROP TABLE IF EXISTS bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info(
     prd_id INT,
@@ -21,17 +23,20 @@ CREATE TABLE bronze.crm_prd_info(
     prd_start_dt DATE,
     prd_end_dt DATE
 );
-
--- crm_sales_details
 DROP TABLE IF EXISTS bronze.crm_sales_details;
+
 CREATE TABLE bronze.crm_sales_details(
-    ID VARCHAR(50),
-    CAT VARCHAR(50),
-    SUBCAT VARCHAR(50),
-    MAINTENANCE VARCHAR(50)
+   sls_ord_num VARCHAR(50),
+   sls_prd_key VARCHAR(50),
+   sls_cust_id INT,
+   sls_order_dt INT,
+   sls_ship_dt INT,
+   sls_due_dt INT,
+   sls_sales INT,
+   sls_quantity INT,
+   sls_price INT
 );
 
--- erp_CUST_AZ12
 DROP TABLE IF EXISTS bronze.erp_CUST_AZ12;
 CREATE TABLE bronze.erp_CUST_AZ12(
     CID VARCHAR(50),
@@ -39,14 +44,12 @@ CREATE TABLE bronze.erp_CUST_AZ12(
     GEN VARCHAR(50)
 );
 
--- erp_LOC_A101
 DROP TABLE IF EXISTS bronze.erp_LOC_A101;
 CREATE TABLE bronze.erp_LOC_A101(
     CID VARCHAR(50),
     CNTRY VARCHAR(50)
 );
 
--- erp_PX_CAT_G1V2
 DROP TABLE IF EXISTS bronze.erp_PX_CAT_G1V2;
 CREATE TABLE bronze.erp_PX_CAT_G1V2(
     ID VARCHAR(50),
